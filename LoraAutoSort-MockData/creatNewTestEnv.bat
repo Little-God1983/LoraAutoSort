72171dbdd04aaa3468eb0040df2a3bee7fbf6af8
Chris <little-god@web.de>
2024-10-27 19:08:38 +0100

    updated LabScript

--- LoraAutoSort-MockData/creatNewTestEnv.bat
+++ LoraAutoSort-MockData/creatNewTestEnv.bat
@@ -1,29 +1,34 @@
 @echo off
+set "basePath=D:\SoftwareTest"
+cd /D "%basePath%"
 
 :: Check if LAB folder exists, then delete it
-if exist "LAB" (
+if exist "%basePath%\LAB" (
     echo Deleting folder LAB...
-    rmdir /s /q "LAB"
+    rmdir /s /q "%basePath%\LAB"
+) else (
+    echo Folder LAB does not exist, skipping delete step.
 )
 
 :: Check if Targer folder exists, then delete it
-if exist "Target" (
-    echo Deleting folder Target...
-    rmdir /s /q "Target"
-)
-
-:: Check if _FreshLab folder exists, then copy it to create LAB
-if exist "_FreshLab" (
-    echo Creating a copy of _FreshLab as LAB...
-    xcopy "_FreshLab" "LAB" /e /i
+if exist "%basePath%\Targer" (
+    echo Deleting folder Targer...
+    rmdir /s /q "%basePath%\Targer"
 ) else (
-    echo The folder _FreshLab does not exist. Cannot create LAB.
+    echo Folder Targer does not exist, skipping delete step.
 )
 
 :: Create a new Targer folder
-echo Creating new Targer folder...
-mkdir "%basePath%\Targer"
+echo Creating new Target folder...
+mkdir "%basePath%\Target"
 
+:: Check if _FreshLab folder exists, then copy it to create LAB
+if exist "%basePath%\_FreshLab" (
+    echo Creating a copy of _FreshLab as LAB...
+    xcopy "%basePath%\_FreshLab" "%basePath%\LAB" /e /i
+) else (
+    echo The folder _FreshLab does not exist. Cannot create LAB.
+)
 
 echo Operation completed.
 pause

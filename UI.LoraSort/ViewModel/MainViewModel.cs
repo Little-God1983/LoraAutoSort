c700186ea5c55777d318ffdb23cec9204e037931
Chris <little-god@web.de>
2025-02-20 22:16:54 +0100

    log can now be filtered to show only errors

--- UI.LoraSort/ViewModel/MainViewModel.cs
+++ UI.LoraSort/ViewModel/MainViewModel.cs
@@ -1,20 +1,57 @@
-﻿using Services.LoraAutoSort.Classes;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
 using System.Windows.Input;
+using Services.LoraAutoSort.Classes;
 
 namespace UI.LoraSort.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        // Existing custom mapping properties
         public ObservableCollection<CustomTagMap> CustomTagMappings { get; set; } = new ObservableCollection<CustomTagMap>();
-
         public ICommand MoveUpCommand { get; }
         public ICommand MoveDownCommand { get; }
+
+        // New logging properties
+        public ObservableCollection<ProgressReport> LogEntries { get; } = new ObservableCollection<ProgressReport>();
+        public ICollectionView LogEntriesView { get; }
+
+        private bool _showErrorsOnly;
+        public bool ShowErrorsOnly
+        {
+            get => _showErrorsOnly;
+            set
+            {
+                if (_showErrorsOnly != value)
+                {
+                    _showErrorsOnly = value;
+                    OnPropertyChanged(nameof(ShowErrorsOnly));
+                    LogEntriesView.Refresh();
+                }
+            }
+        }
 
         public MainViewModel()
         {
             MoveUpCommand = new RelayCommand<CustomTagMap>(MoveMappingUp);
             MoveDownCommand = new RelayCommand<CustomTagMap>(MoveMappingDown);
+
+            // Set up the filtered view for logs.
+            LogEntriesView = CollectionViewSource.GetDefaultView(LogEntries);
+            LogEntriesView.Filter = o =>
+            {
+                if (o is ProgressReport report)
+                {
+                    if (ShowErrorsOnly)
+                    {
+                        // Only show errors (IsSuccessful explicitly false)
+                        return report.IsSuccessful.HasValue && !report.IsSuccessful.Value;
+                    }
+                    return true;
+                }
+                return false;
+            };
         }
 
         public void MoveMappingUp(CustomTagMap map)
@@ -43,6 +80,26 @@ namespace UI.LoraSort.ViewModels
             {
                 CustomTagMappings[i].Priority = i;
             }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public void ClearLogs()
+        {
+            LogEntries.Clear();
+        }
+
+        public void AppendLog(string message, bool isError)
+        {
+            LogEntries.Add(new ProgressReport
+            {
+                StatusMessage = message,
+                // Assuming that an error should be indicated by IsSuccessful == false.
+                IsSuccessful = !isError
+            });
         }
     }
 }

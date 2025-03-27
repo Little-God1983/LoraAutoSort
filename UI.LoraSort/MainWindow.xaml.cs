1c3157c0defe23958c49756bc212f459d726ecce
Chris <little-god@web.de>
2025-03-27 22:29:56 +0100

    multi refactoring

--- UI.LoraSort/MainWindow.xaml.cs
+++ UI.LoraSort/MainWindow.xaml.cs
@@ -187,104 +187,110 @@ namespace UI.LoraSort
         {
             if (!_isProcessing)
             {
-                // Switch UI states
-                _isProcessing = true;
-                btnGoCancel.Content = "Cancel"; // Button shows "Cancel" now
-                ((MainViewModel)DataContext).ClearLogs(); // Clear old logs
-                btnTargetPath.IsEnabled = false;
-                btnBasePath.IsEnabled = false;
+                await StartProcessingAsync();
+            }
+            else
+            {
+                _cts?.Cancel();
+            }
+        }
 
-                // Create a new CTS for this run
-                _cts = new CancellationTokenSource();
-                if (String.IsNullOrEmpty(txtBasePath.Text) || String.IsNullOrEmpty(txtTargetPath.Text))
+        private async Task StartProcessingAsync()
+        {
+            try
+            {
+                SetProcessingUIState();
+
+                if (!ValidatePaths())
                 {
-                    MessageBox.Show("No path selected", "No Path", MessageBoxButton.OK, MessageBoxImage.Warning, MessageBoxResult.Yes);
-                    ResetUI();
+                    ShowMessageAndResetUI("No path selected", "No Path");
                     return;
                 }
-
 
                 if (IsPathTheSame())
                 {
-                    MessageBox.Show("Select a different target than the source path.", "Source cannot be targe path", MessageBoxButton.OK, MessageBoxImage.Warning, MessageBoxResult.Yes);
-                    ResetUI();
+                    ShowMessageAndResetUI("Select a different target than the source path.", "Source cannot be target path");
                     return;
                 }
-                FileControllerService controllerService = new FileControllerService();
 
+                var controllerService = new FileControllerService();
                 if ((bool)radioCopy.IsChecked && !controllerService.EnoughFreeSpaceOnDisk(txtBasePath.Text, txtTargetPath.Text))
                 {
-                    MessageBox.Show("You don't have enough disk space to copy the files.", "Insuficcent Diskspace", MessageBoxButton.OK, MessageBoxImage.Warning, MessageBoxResult.Yes);
-                    ResetUI();
+                    ShowMessageAndResetUI("You don't have enough disk space to copy the files.", "Insuficcent Diskspace");
                     return;
                 }
-
-                ((MainViewModel)DataContext).ClearLogs(); // Clear previous entries
 
                 bool moveOperation = false;
                 if (!(bool)radioCopy.IsChecked)
                 {
                     if (!ShowConfirmationDialog("Moving instead of copying means that the original file order cannot be restored. Continue anyways?", "Are you sure?"))
                     {
+                        ResetUI();
                         return;
                     }
-                    else
-                    {
-                        moveOperation = true;
-                    }
+                    moveOperation = true;
                 }
 
-                try
+                var progressIndicator = CreateProgressIndicator();
+                await controllerService.ComputeFolder(progressIndicator, _cts.Token, new SelectedOptions()
                 {
-                    var progressIndicator = new Progress<ProgressReport>(report =>
-                    {
-                        // Even though Progress<T> should already marshal back to the UI thread,
-                        // we explicitly ensure it by using the Dispatcher.
-                        Application.Current.Dispatcher.Invoke(() =>
-                        {
-                            if (report.Percentage.HasValue)
-                            {
-                                progressBar.Value = report.Percentage.Value;
-                            }
-                            txtStatus.Text = report.StatusMessage;
-                            bool isError = report.IsSuccessful.HasValue && !report.IsSuccessful.Value;
-                            // Add the report to your log collection in the view model:
-                            var vm = DataContext as MainViewModel;
-                            vm?.LogEntries.Add(report);
-                        });
-                    });
-
-                    await controllerService.ComputeFolder(progressIndicator, cancellationToken: _cts.Token, new SelectedOptions()
-                    {
-                        BasePath = txtBasePath.Text,
-                        TargetPath = txtTargetPath.Text,
-                        IsMoveOperation = moveOperation,
-                        OverrideFiles = (bool)chbOverride.IsChecked,
-                        CreateBaseFolders = (bool)chbBaseFolders.IsChecked
-                    });
-                    //await controllerService.ComputeFolder(progressIndicator, txtBasePath.Text, txtTargetPath.Text, moveOperation, (bool)chbOverride.IsChecked, cancellationToken: _cts.Token, (bool)chbNoBaseFolders.IsChecked);
-                }
-                catch (OperationCanceledException)
-                {
-                    // This is expected if user cancels
-                    ((MainViewModel)DataContext).AppendLog("Operation was canceled by user.", isError: false);
-                }
-                catch (Exception ex)
-                {
-                    // Other exceptions
-                    Log.Error($"Unexpected error: {ex.Message}");
-                }
-                finally
-                {
-                    // Reset UI
-                    ResetUI();
-                }
-                // This is executed on the UI thread.
+                    BasePath = txtBasePath.Text,
+                    TargetPath = txtTargetPath.Text,
+                    IsMoveOperation = moveOperation,
+                    OverrideFiles = (bool)chbOverride.IsChecked,
+                    CreateBaseFolders = (bool)chbBaseFolders.IsChecked
+                });
             }
-            else
+            catch (OperationCanceledException)
             {
-                _cts.Cancel();
+                ((MainViewModel)DataContext).AppendLog("Operation was canceled by user.", isError: false);
             }
+            catch (Exception ex)
+            {
+                Log.Error($"Unexpected error: {ex.Message}");
+            }
+            finally
+            {
+                ResetUI();
+            }
+        }
+
+        private bool ValidatePaths()
+        {
+            return !string.IsNullOrEmpty(txtBasePath.Text) && !string.IsNullOrEmpty(txtTargetPath.Text);
+        }
+
+        private void ShowMessageAndResetUI(string message, string caption)
+        {
+            MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Warning);
+            ResetUI();
+        }
+
+        private void SetProcessingUIState()
+        {
+            _isProcessing = true;
+            btnGoCancel.Content = "Cancel";
+            ((MainViewModel)DataContext).ClearLogs();
+            btnTargetPath.IsEnabled = false;
+            btnBasePath.IsEnabled = false;
+            _cts = new CancellationTokenSource();
+        }
+
+        private IProgress<ProgressReport> CreateProgressIndicator()
+        {
+            return new Progress<ProgressReport>(report =>
+            {
+                // Ensure UI updates are on the UI thread.
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    if (report.Percentage.HasValue)
+                    {
+                        progressBar.Value = report.Percentage.Value;
+                    }
+                    txtStatus.Text = report.StatusMessage;
+                    ((MainViewModel)DataContext)?.LogEntries.Add(report);
+                });
+            });
         }
 
         private void ResetUI()
